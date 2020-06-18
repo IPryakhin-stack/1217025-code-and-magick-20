bfb2c40e5517c8205649ddb2eac751f98c6e35c4
Илья Пряхин <pryakhinilya626@gmail.com>
2020-06-18 23:25:23 +0300

    Разбор замечаний

--- js/stat.js
+++ js/stat.js
@@ -15,6 +15,17 @@ var renderCloud = function(ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
+var renderTitle = function(ctx, x, y, gap, font, color) {
+  ctx.font = font;
+  ctx.fillStyle = color;
+  ctx.fillText('Ура вы победили!', x, y + gap);
+  ctx.fillText('Список результатов:', x, y + gap* 2);
+}
+
+var getRandonColor = function(ctx, hue, randomSaturation, lightness) {
+  ctx.fillStyle = 'hsl(hue, randomSaturation * 100%, lightness * 1%)';
+}
+
 var getMaxElement = function(arr) {
   var maxElement = arr[0];
 
@@ -30,20 +41,14 @@ var getMaxElement = function(arr) {
 window.renderStatistics = function(ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP * 2);
+  renderTitle(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, FONT_GAP, '16px PT Mono', '#000')
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
-    var columnColor = (players[i] === 'Вы') ? ctx.fillStyle = 'rgba(255, 0, 0, 1)' : ctx.fillStyle = 'hsl(240,'+ Math.ceil(Math.random() * 100) +'%, 50%)';
+    var columnColor = (players[i] === 'Вы') ? ctx.fillStyle = 'rgba(255, 0, 0, 1)' : getRandonColor(ctx, 240, Math.ceil(Math.random()), 50)
     ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + FONT_GAP * 2 + GAP * 3 + BAR_HEIGHT, BAR_WIDTH, - (BAR_HEIGHT  * times[i]) / maxTime);
-  }
 
-  for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000';
     ctx.fillText(players[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + GAP * 2 + FONT_GAP * 4 + BAR_HEIGHT);
     ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + GAP + FONT_GAP * 3 + BAR_HEIGHT - (BAR_HEIGHT  * times[i]) / maxTime);
